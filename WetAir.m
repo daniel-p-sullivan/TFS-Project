322a3855c48e0b2b5f620b49c5815c92eb1227ed
danielpsullivan94 <daniel.p.sullivan@utexas.edu>
2017-02-25 12:58:07 -0600

    WetAir Phase1
    added phase1 enumeration

--- WetAir.m
+++ WetAir.m
@@ -26,9 +26,9 @@ classdef WetAir
             pwv = rh * psat;
             xwv = 0.622 * (pwv / (pmix - pwv)) / (1 + 0.622 * (pwv / (pmix - pwv)));
             c.X = [1 - xwv, xwv];
-            ywv = pwv / pmix
-            yda = (1 - ywv) * [0.78084, 0.20947, 0.00934 0.00035]
-            y = [yda ywv]
+            ywv = pwv / pmix;
+            yda = (1 - ywv) * [0.78084, 0.20947, 0.00934 0.00035];
+            y = [yda ywv];
             c.Y = y;
             wetair = Mixture([Gas.Nitrogen, Gas.Oxygen, Gas.Argon, Gas.Carbon_Dioxide, Gas.Water], y, 307.6);
             c.MIX = wetair
@@ -37,7 +37,9 @@ classdef WetAir
             c.H = mass_hmix(wetair, tmix);
             c.S = mass_smix(c, 101.325);
             c.U = mass_umix(wetair, tmix);
-        end        
+        end
     end
-    
+    enumeration
+       Phase1 (0.6, 298.1, 99.4017, 2.128); 
+    end
 end
